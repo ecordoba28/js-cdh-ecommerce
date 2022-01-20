6197fdf892ee279df0eea2dcbdf8577ddebafb5f
ecordoba28 <isaacgtfo@gmail.com>
2022-01-20 17:09:54 -0300

    Primer commit añadiendo imagenes y nav simple

---
+++ stock.js
@@ -0,0 +1,26 @@
+let stockProductos = [
+    {id: 1, nombre: "Remera 1", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "S", img: 'img/remera1.jpg', cantidad:1},
+    {id: 2, nombre: "Remera 2", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "S", img: 'img/remera2.jpg', cantidad:1},
+    {id: 3, nombre: "Remera 3", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "M", img: 'img/remera3.jpg', cantidad:1},
+    {id: 4, nombre: "Remera 4", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "M", img: 'img/remera4.jpg', cantidad:1},
+    {id: 5, nombre: "Remera 5", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "L", img: 'img/remera5.jpg', cantidad:1},
+    {id: 6, nombre: "Remera 6", tipo: "remera", desc: "Una remera que re va con vos", precio: 2800, talle: "XL", img: 'img/remera6.jpg', cantidad:1},
+    {id: 7, nombre: "Buzo 1", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 3900, talle: "M", img: 'img/buzo1.jpg', cantidad:1},
+    {id: 8, nombre: "Buzo 2", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 3900, talle: "L", img: 'img/buzo2.jpg', cantidad:1},
+    {id: 9, nombre: "Buzo 3", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 3900, talle: "XL", img: 'img/buzo3.jpg', cantidad:1},
+    {id: 10, nombre: "Buzo 4", tipo: "buzo", desc: "Un buzo que re va con vos", precio: 1200, talle: "S", img: 'img/remera1.jpg', cantidad:1},
+    {id: 11, nombre: "Pantalon 1", tipo: "pantalon", desc: "Un pantalon que re va con vos", precio: 1200, talle: "S", img: 'img/pant1.jpg', cantidad:1},
+    {id: 12, nombre: "Pantalon 2", tipo: "pantalon", desc: "Una pantalon que re va con vos", precio: 800, talle: "M", img: '/img/pant2.jpg', cantidad:1},
+    {id: 13, nombre: "Pantalon 3", tipo: "pantalon", desc: "Una pantalon que re va con vos", precio: 800, talle: "L", img: '/img/pant3.jpg', cantidad:1},
+    {id: 14, nombre: "Zapatillas 1", tipo: "zapatillas", desc: "Unas zapas que re van con vos", precio: 5400, talle: "otros", img: '/img/zap1.jpg', cantidad:1},
+    {id: 15, nombre: "Zapatillas 2", tipo: "zapatillas", desc: "Unas zapas que re van con vos", precio: 5400, talle: "otros", img: '/img/zap2.jpg', cantidad:1},
+    {id: 16, nombre: "Zapatillas 3", tipo: "zapatillas", desc: "Unas zapas que re van con vos", precio: 5400, talle: "otros", img: '/img/zap3.jpg', cantidad:1},
+    {id: 17, nombre: "Zapatillas 4", tipo: "zapatillas", desc: "Unas zapas que re van con vos", precio: 5400, talle: "otros", img: '/img/zap4.jpg', cantidad:1},
+    {id: 18, nombre: "Mochila 1", tipo: "mochila", desc: "Una mochila que re va con vos", precio: 5400, talle: "otros", img: '/img/mochi1.jpg', cantidad:1},
+    {id: 19, nombre: "Mochila 2", tipo: "mochila", desc: "Una mochila que re va con vos", precio: 5400, talle: "otros", img: '/img/mochi2.jpg', cantidad:1},
+    {id: 20, nombre: "Gorra 1", tipo: "gorra", desc: "Una gorra que re va con vos", precio: 1500, talle: "otros", img: '/img/gorra1.jpg', cantidad:1},
+
+
+
+
+]
