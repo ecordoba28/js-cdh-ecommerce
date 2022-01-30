16838724c82c77a08e0a346c0d2b4c2e38cbfd5f
ADM <ADM@A-ANET-81022.ALMAGRO.ort.arg>
2022-01-29 21:11:04 -0300

    commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 
 const productContainer = document.getElementById('productContainer');
+const cart = [];
 
 
 
-
+showProducts(productStock);
 
 function showProducts(array){
     productContainer.innerHTML = ''
@@ -20,14 +21,47 @@ function showProducts(array){
                 <p class="card-text">${products.desc}</p>
                 <p class="card-text">Talle: ${products.size}</p>
                 <p class="card-text">$${products.price}</p>
-                <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                <a id= "${products.id}" class="btn btn-primary">Añadir al carrito</a>
             </div>
         </div>
         `
 
         productContainer.appendChild(div)
+
+        let addButton = document.getElementById(`${products.id}`)
+        addButton.addEventListener('click', () => {
+            addToCart();
+        })
     });
 }
 
 
-showProducts(productStock);
+
+
+function addToCart(id){
+    let add = productStock.find(product => product.id == id)
+
+    cart.push(add)
+
+    let div = document.createElement('div')
+    div.classList.add('productOnCart')
+    div.innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+
+}
+
